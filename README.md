531b52f93c5fb08203adba8a11d4af902196abb7
Josh Cheek <josh.cheek@gmail.com>
2019-03-01 05:47:36 -0600

    Not sure why I waited so long to commit, but better late than never

---
+++ README.md
@@ -0,0 +1,13 @@
+Implementing the Observable API as a learning experience.
+
+Install dependencies:
+
+```sh
+npm install
+```
+
+Run tests:
+
+```sh
+node index.js | less -R
+```
---
+++ index.js
@@ -0,0 +1,131 @@
+const util = require('util')
+
+const pEmitter   = Symbol('emitter')
+const pClosed    = Symbol('closed')
+const pCleanupFn = Symbol('cleanupFn')
+const pNext      = Symbol('next')
+const pError     = Symbol('error')
+const pComplete  = Symbol('complete')
+const pObserver  = Symbol('observer')
+
+const noopFn  = (val) => {}
+const throwFn = (err) => { throw err }
+
+function inspect(val) {
+  return util.inspect(val, { colors: true })
+}
+
+Symbol.observable = Symbol.for("observable")
+
+class Subscription {
+  constructor(emitter, observer) {
+    this.constructor = Object // fkn whatever...
+    this[pClosed]    = false
+    this[pObserver]  = observer
+    // this[pNext]      = nextFn
+    // this[pError]     = errorFn
+    // this[pComplete]  = completeFn
+
+    // { start: startFn,
+    //     next: nextFn,
+    //     error: errorFn,
+    //     complete: completeFn
+    // }
+
+    let needsCleanup = false
+    let cleanup = function() { needsCleanup = true }
+    this[pCleanupFn] = cleanup
+
+    let tmp
+    try {
+      if(observer.start)
+        observer.start(this)
+
+      if(this.closed)
+        return
+
+      tmp = emitter({
+        next: function(val) {
+          if(observer.next) observer.next(val)
+        },
+        error: function(err) {
+          cleanup()
+          if(observer.error) observer.error(err)
+          else throw err
+        },
+        complete: function(val) {
+          cleanup()
+          if(observer.complete) observer.complete(val)
+        }
+      })
+    } catch(e) {
+      ;(observer.error || throwFn)(e)
+      return
+    }
+
+    if('object' === typeof tmp && tmp !== null && 'function' === typeof tmp.unsubscribe) {
+      const subscription = tmp
+      tmp = () => subscription.unsubscribe()
+    }
+
+    if('function' !== typeof tmp && tmp !== null && tmp !== undefined)
+      throw new TypeError(`Invalid cleanup function: ${inspect(tmp)}`)
+
+    this[pCleanupFn] = cleanup = (tmp || noopFn)
+
+    if(needsCleanup)
+      cleanup()
+  }
+
+  unsubscribe() {
+    if(this[pClosed]) return
+    this[pCleanupFn]()
+    this[pClosed] = true
+  }
+
+  get closed() {
+    return this[pClosed]
+  }
+}
+
+class MyObservable {
+  constructor(emitter) {
+    if("function" !== typeof emitter)
+      throw new TypeError("First arg must be callable")
+    this[pEmitter] = emitter
+  }
+
+  subscribe(observer) {
+    if(!observer || !('object' === typeof observer || 'function' === typeof observer) )
+      throw new TypeError("First arg must be an object or the onNext function")
+
+    if('function' === typeof observer) {
+      const { 0: onNext, 1: onError, 2: onComplete } = arguments
+      observer = {
+        next:     onNext,
+        error:    onError,
+        complete: onComplete,
+      }
+    }
+    return new Subscription(this[pEmitter], observer)
+
+//     observer.start    = (observer.start    || noopFn)
+//     observer.next     = (observer.next     || noopFn)
+//     observer.error    = (observer.error    || throwFn)
+//     observer.complete = (observer.complete || noopFn)
+
+  }
+
+  [Symbol.observable]() {
+  }
+}
+
+// const o = new MyObservable(
+//   function(idk) {
+//   }
+// )
+// console.log(
+//   o[Symbol.observable],
+//   Symbol.observable
+// )
+require("es-observable-tests").runTests(MyObservable);
